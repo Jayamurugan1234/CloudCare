c126a970986361bdb3a6ebbdd50ec0ef748fa7d4
Jayamurugan1234 <jayamuruganvenkatachalam27@gmail.com>
2026-07-23 12:58:01 +0530

    Update axios config

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://cloudcare-backend.onrender.com/api",
+  baseURL: "https://cloudcare-backend-m0uf.onrender.com/",
 });
 
 api.interceptors.request.use((config) => {
